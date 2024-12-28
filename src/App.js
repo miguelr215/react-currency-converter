741027405269b0a6c9016318cc70094d33466f89
miguelr215 <drexelramos@gmail.com>
2024-12-28 18:01:45 -0500

    converted amt

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ export default function App() {
 	const [inputQuery, setInputQuery] = useState(1);
 	const [inputCountry, setInputCountry] = useState('USD');
 	const [outputCountry, setOutputCountry] = useState('EUR');
+	const [convertedAmt, setConvertedAmt] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
 	function handleSetInputCountry(country) {
 		setInputCountry(country);
@@ -48,6 +50,11 @@ export default function App() {
 		function () {
 			async function getFX() {
 				try {
+					if (inputCountry === outputCountry) {
+						setConvertedAmt(inputQuery);
+						return;
+					}
+					setIsLoading(true);
 					const res = await fetch(
 						`https://api.frankfurter.dev/v1/1999-01-04?base=${inputCountry}&symbols=${outputCountry}`
 					);
@@ -62,10 +69,8 @@ export default function App() {
 					console.log('rates:', data);
 
 					const converted = inputQuery * data.rates[outputCountry];
-					console.log('converted:', converted.toFixed(2));
-
-					document.querySelector('.output').innerText =
-						converted.toFixed(2);
+					setConvertedAmt(converted.toFixed(2));
+					setIsLoading(false);
 				} catch (error) {
 					console.log(error);
 				}
@@ -85,19 +90,22 @@ export default function App() {
 					<CurrencyInput
 						inputQuery={inputQuery}
 						setInputQuery={setInputQuery}
+						disabled={isLoading}
 					/>
 					<CountrySelection
 						countries={countries}
 						country={inputCountry}
 						setInput={handleSetInputCountry}
+						disabled={isLoading}
 					/>
 				</div>
 				<div className="output-wrapper">
-					<p className="output">OUTPUT</p>
+					<p className="output">{convertedAmt}</p>
 					<CountrySelection
 						countries={countries}
 						country={outputCountry}
 						setInput={handleSetOutputCountry}
+						disabled={isLoading}
 					/>
 				</div>
 			</div>
@@ -105,18 +113,19 @@ export default function App() {
 	);
 }
 
-function CurrencyInput({ inputQuery, setInputQuery }) {
+function CurrencyInput({ inputQuery, setInputQuery, disabled }) {
 	return (
 		<input
 			type="number"
 			className="currency-input"
 			value={inputQuery}
-			onChange={(e) => setInputQuery(e.target.value)}
+			onChange={(e) => setInputQuery(Number(e.target.value).toFixed(2))}
+			disabled={disabled}
 		/>
 	);
 }
 
-function CountrySelection({ countries, country, setInput }) {
+function CountrySelection({ countries, country, setInput, disabled }) {
 	const countriesList = Object.keys(countries);
 
 	return (
@@ -124,6 +133,7 @@ function CountrySelection({ countries, country, setInput }) {
 			className="countries"
 			value={country}
 			onChange={(e) => setInput(e.target.value)}
+			disabled={disabled}
 		>
 			{countriesList.map((country) => (
 				<option value={country} key={countriesList.indexOf(country)}>
